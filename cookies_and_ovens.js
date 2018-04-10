2383c73c0ac36553490b2e61a0c79bce96df8f01
ihsanmaulanahamdani <ihsanmaulana5@gmail.com>
2018-04-10 15:26:13 +0700

    done

--- cookies_and_ovens.js
+++ cookies_and_ovens.js
@@ -8,3 +8,69 @@
 //
 //
 // Your code here
+
+class Cookies {
+    constructor(name, time, status) {
+        this.cake_name = name;
+        this.cake_time = time;
+        this.cake_status = status;
+    }
+}
+
+class PeanutCake extends Cookies {
+    constructor() {
+        super('Peanut Cake', 30, 'mentah');
+    }
+}
+
+class ChocolateCake extends Cookies {
+    constructor() {
+        super('Chocolate Cake', 20, 'mentah');
+    }
+}
+
+class CheeseCake extends Cookies {
+    constructor() {
+        super('Cheese Cake', 35, 'mentah');
+    }
+}
+
+class Oven {
+    constructor(name, time) {
+        this.cake = name;
+        this.baking_time = time;
+        this.arrayOfCake = [];
+    }
+
+    bake() {
+        for (let i = 0; i < this.baking_time; i += 5) {
+            if (this.cake.cake_name === 'Peanut Cake') {
+                this.cake.cake_name = 'Kue Kacang';
+            } else if (this.cake.cake_name === 'Chocolate Cake') {
+                this.cake.cake_name = 'Kue Coklat';
+            } else if (this.cake.cake_name === 'Cheese Cake') {
+                this.cake.cake_name = 'Kue Keju';
+            }
+
+            if (i + 5 < 15) {
+                this.arrayOfCake.push(`${this.cake.cake_name}, menit ke ${i+5} : ${this.cake.cake_status}`);
+            } else if (i + 5 < this.cake.cake_time && i + 5 >= 15) {
+                this.arrayOfCake.push(`${this.cake.cake_name}, menit ke ${i+5} : hampir matang`);
+            } else if (i + 5 === this.cake.cake_time) {
+                this.arrayOfCake.push(`${this.cake.cake_name}, menit ke ${i+5} : matang`);
+            } else if (i + 5 > this.cake.cake_time) {
+                this.arrayOfCake.push(`${this.cake.cake_name}, menit ke ${i+5} : hangus`);
+            }
+        }
+
+        return this.arrayOfCake.join('\n');
+    }
+}
+
+let peanut_cake = new PeanutCake();
+let chocolate_cake = new ChocolateCake();
+let cheese_cake = new CheeseCake();
+
+let oven = new Oven(chocolate_cake, 25);
+
+console.log(oven.bake());
