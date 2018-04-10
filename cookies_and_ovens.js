e956371684b20cd5b782d756a5a18e5e39d95ced
asepridwan1989 <aseprmail@gmail.com>
2018-04-10 14:43:42 +0700

    done

--- cookies_and_ovens.js
+++ cookies_and_ovens.js
@@ -8,3 +8,55 @@
 //
 //
 // Your code here
+
+class Oven{
+    static bake(kue,timeBake){
+      let time = 0
+      while(time <timeBake){
+        time +=5
+        if(time === kue.matang){
+          kue.status = 'matang'
+        }else if(time <= kue.matang/2){
+          kue.status = 'mentah'
+        }else if(time < kue.matang){
+          kue.status = 'hampir matang'
+        }else{
+            kue.status = 'hangus'
+        }
+        console.log(`${kue.name}, menit ke ${time} : ${kue.status}`)
+      }
+    }
+}
+
+class Cookie{
+  constructor(name,matang){
+      this.name = name
+      this.matang = matang
+      this.status = 'mentah'
+  }
+}
+
+class Kacang extends Cookie{
+  constructor(){
+    super('Kue kacang',30)
+  }
+}
+
+class Coklat extends Cookie{
+  constructor(){
+    super('Kue coklat', 20)
+  }
+}
+
+class Keju extends Cookie{
+  constructor(){
+    super('Kue keju',35)
+  }
+}
+
+let kacang = new Kacang()
+Oven.bake(kacang,30)
+let coklat = new Coklat()
+Oven.bake(coklat,25)
+let keju = new Keju()
+Oven.bake(keju,25)
