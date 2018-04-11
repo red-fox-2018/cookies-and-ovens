ced01b752d4c411e793abadfb14f63661e2e5e8d
anugrah94 <anugrahrezkifauzinasution@gmail.com>
2018-04-11 18:33:05 +0700

    selesai anchor cookies and ovens

--- cookies_and_ovens.js
+++ cookies_and_ovens.js
@@ -8,3 +8,79 @@
 //
 //
 // Your code here
+
+
+
+
+
+
+// class Cookies{
+//     constructor(){
+//         this.cookieName = null
+//         this.cookieCondition = 'mentah'
+//         this.halfFinish = null
+//         this.timeToFinishCook = null
+//     }
+// }
+
+class PeanutCookie{
+    constructor(){
+        // super()
+        this.cookieName = 'Peanut Cookie'
+        this.cookieCondition = 'mentah'
+        this.halfFinish = 15
+        this.timeToFinishCook = 20
+    }
+}
+
+class ChocolateCookie{
+    constructor(){
+        // super()
+        this.cookieName = 'Chocolate Cookie'
+        this.cookieCondition = 'mentah'
+        this.halfFinish = 25
+        this.timeToFinishCook = 30
+    }
+}
+
+class CheeseCookie{
+    constructor(){
+        // super()
+        this.cookieName = 'Cheese Cookie'
+        this.cookieCondition = 'mentah'
+        this.halfFinish = 30
+        this.timeToFinishCook = 35
+    }
+}
+
+let peanutCookie = new PeanutCookie()
+let chocolateCookie = new ChocolateCookie()
+let cheeseCookie = new CheeseCookie()
+
+class Oven{
+    
+    static bake(time, cookie){
+        let bakingTime = 0;
+        while(bakingTime <= time){
+            if(bakingTime === cookie.halfFinish){
+                cookie.cookieCondition = 'setengah matang'
+            }
+            else if(bakingTime === cookie.timeToFinishCook){
+                cookie.cookieCondition = 'matang'
+            }
+            else if(bakingTime > cookie.timeToFinishCook){
+                cookie.cookieCondition = 'hangus'
+            }
+            else{
+                cookie.cookieCondition = 'mentah'
+            }
+            console.log(cookie.cookieName+', '+' menit ke '+bakingTime+' : '+cookie.cookieCondition)
+            bakingTime += 5;
+        }
+    }
+
+}
+
+Oven.bake(35,peanutCookie)
+// Oven.bake(25,chocolateCookie)
+// Oven.bake(30,cheeseCookie)
