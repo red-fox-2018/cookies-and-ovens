e19183c2c8165c7d548d34bbf6bda53e4451bc00
Natascha Lamsu <tazcha15@gmail.com>
2018-04-11 08:50:25 +0700

    cookies and oven

--- cookies_and_ovens.js
+++ cookies_and_ovens.js
@@ -8,3 +8,65 @@
 //
 //
 // Your code here
+
+
+
+class Kue{
+  constructor(name, timer) {
+    this.name = name
+    this.timer = timer
+    this.status = 'mentah'
+  }
+}
+
+class KueCoklat extends Kue{
+  constructor() {
+    super('Kue coklat', 20)
+  }
+}
+
+class KueKacang extends Kue{
+  constructor() {
+    super('Kue kacang', 30)
+  }
+}
+
+class KueKeju extends Kue{
+  constructor() {
+    super('Kue keju', 35)
+  }
+}
+
+class Oven{
+  constructor() {
+    this.tray = []
+  }
+
+  onTray(objCookie) {
+    this.tray.push(objCookie)
+  }
+
+  bake(time) {
+    var menit = 5;
+    for(var j = 5; j <= time; j+=5) {
+      for(let i in this.tray) {
+        if(j >= 10 && j < this.tray[i].timer) {
+          this.tray[i].status = 'hampir matang'
+        } else if(j === this.tray[i].timer) {
+          this.tray[i].status = 'matang'
+        } else if(j > this.tray[i].timer){
+          this.tray[i].status = 'hangus'
+        }
+        console.log(`${this.tray[i].name}, menit ke ${j} : ${this.tray[i].status}`)
+      }
+    }
+  }
+
+}
+
+var kueCoklat = new KueCoklat()
+var kueKeju = new KueKeju()
+// console.log(kueCoklat);
+var baked = new Oven()
+baked.onTray(kueCoklat)
+baked.bake(30);
