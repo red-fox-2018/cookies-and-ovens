e7626f8936e98c06717a3765a2b495659c637038
cantdocpp <cihuyman95@gmail.com>
2018-04-11 23:22:36 +0700

    first commit

--- cookies_and_ovens.js
+++ cookies_and_ovens.js
@@ -8,3 +8,50 @@
 //
 //
 // Your code here
+class Kue {
+  constructor(name, waktuMasak) {
+    this.name = name;
+    this.waktuMasak = waktuMasak;
+    this.status = 'mentah';
+  }
+
+  masak() {
+    for (var i = 0; i <= this.waktuMasak + 5; i+=5) {
+      if (i > 10 && i <= 15) {
+        this.status = 'hampir matang'
+      } else if (i == this.waktuMasak) {
+        this.status = 'matang';
+      } else if (i >= this.waktuMasak) {
+        this.status = 'hangus'
+      }
+      console.log(`${this.name}, menit ke ${i} : ${this.status}`);
+    }
+  }
+}
+
+class KueCokelat extends Kue {
+  constructor(name, waktuMasak) {
+    super(name, waktuMasak);
+  }
+
+
+}
+
+class Kuekacang extends Kue {
+  constructor(name, waktuMasak) {
+    super(name, waktuMasak);
+  }
+}
+
+class KueKeju extends Kue {
+  constructor(name, waktuMasak) {
+    super(name, waktuMasak);
+  }
+}
+
+var kueCokelat = new KueCokelat('kue cokelat', 20);
+var kuekacang = new Kuekacang('kue kacang', 30);
+var kueKeju = new KueKeju('kue keju', 35);
+
+kueCokelat.masak();
+kuekacang.masak()
