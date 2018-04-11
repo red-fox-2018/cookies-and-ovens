23cdf17c3a6124c9d28dff5d49893383b696cbf9
NeilTanado <neiltanado@yahoo.co.id>
2018-04-11 16:52:29 +0700

    first push

--- cookies_and_ovens.js
+++ cookies_and_ovens.js
@@ -1,10 +1,51 @@
-// Answer These Questions:
-//
-// - What are essential classes?
-// - What attributes will each class have?
-// - What interface will each class provide?
-// - How will the classes interact with each other?
-// - Which classes will inherit from others, if any?
-//
-//
-// Your code here
+/*jshint esversion:6*/
+
+class Cake {
+  constructor(name, timeCooked) {
+    this.name = name;
+    this.timeBake = 0;
+    this.timeCooked = timeCooked;
+    this.status = 'mentah';
+  }
+}
+
+class ChocolateCake extends Cake {
+  constructor(name, timeCooked) {
+    super(name, 20);
+  }
+}
+
+class PeanutCake extends Cake {
+  constructor(name, timeCooked) {
+    super(name, 30);
+  }
+}
+
+class CheeseCake extends Cake {
+  constructor(name, timeCooked) {
+    super(name, 35);
+  }
+}
+
+class Oven {
+  static bake(kue, time) {
+    for(let i=5; i<=time; i+=5) {
+      if(i > kue.timeCooked) {
+        kue.status = 'hangus';
+        console.log(`${kue.name}, menit ke ${i} : ${kue.status}`);
+      } else if(i == kue.timeCooked) {
+        kue.status = 'matang';
+        console.log(`${kue.name}, menit ke ${i} : ${kue.status}`);
+      } else {
+        console.log(`${kue.name}, menit ke ${i} : ${kue.status}`);
+      }
+    }
+  }
+}
+
+
+let coklat = new ChocolateCake('Chocolate Cake');
+let kacang = new PeanutCake('Peanut Cake');
+let keju = new CheeseCake('Cheese Cake');
+
+Oven.bake(kacang, 30);
